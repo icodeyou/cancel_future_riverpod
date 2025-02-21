e05b7d55a3d1ce924b0070613987f899107e0408
Jean Vallon <jvallon44@gmail.com>
2025-02-21 19:11:27 +0100

    first commit maggle

---
+++ android/app/src/main/kotlin/com/example/cancel_future_riverpod/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cancel_future_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.g.dart
@@ -0,0 +1,42 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$futureControllerHash() => r'fcc6680dbd558b75848b93687127e46afea772d9';
+
+/// See also [FutureController].
+@ProviderFor(FutureController)
+final futureControllerProvider =
+    AutoDisposeNotifierProvider<FutureController, void>.internal(
+  FutureController.new,
+  name: r'futureControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$futureControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FutureController = AutoDisposeNotifier<void>;
+String _$mainControllerHash() => r'041e774f569822a8c08c6310f0de42cba957c252';
+
+/// See also [MainController].
+@ProviderFor(MainController)
+final mainControllerProvider =
+    AutoDisposeAsyncNotifierProvider<MainController, void>.internal(
+  MainController.new,
+  name: r'mainControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$mainControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MainController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
